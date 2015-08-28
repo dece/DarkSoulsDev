b12116b81f2d165764592ff214bb8e973953d68a
Shgck <shgck@pistache.land>
2015-08-28 20:29:41 +0200

    Collect amount of each type of file with collect_extensions

--- Scripts/collect_extensions.py
+++ Scripts/collect_extensions.py
@@ -1,15 +1,19 @@
 import os
 
 
-output_dir = r"F:\Dev\Projets\DarkSoulsDev\Output"
+output_dir = r"F:\Dev\Projets\DarkSoulsDev\ExtractedSounds"
 
-extensions = set()
+extensions = {}
+
 
 for root, dirs, files in os.walk(output_dir):
     for file_name in files:
         ext = os.path.splitext(file_name)[1]
         ext = ext.lstrip(".")
-        extensions.add(ext)
+        if ext in extensions:
+            extensions[ext] += 1
+        else:
+            extensions[ext] = 0
 
-for ext in sorted(extensions):
-    print(ext)
+for ext in sorted(list(extensions.keys())):
+    print(ext, ":", extensions[ext])
