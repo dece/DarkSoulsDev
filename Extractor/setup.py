2b8f07d73394fe87cf792a03f6a570f7bcd425d1
Shgck <shgck@pistache.land>
2015-08-29 19:01:44 +0200

    Setup script for dks_archives

---
+++ Extractor/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name = "DKS Archives",
+    version = "1.0",
+    description = "Dark Souls archives parser (BDT/BHD5/BHF, BND, DCX)",
+    author = "Shgck",
+    author_email = "shgck@pistache.land",
+    url = "https://gitlab.com/Shgck/dark-souls-dev",
+
+    packages = ["dks_archives"]
+)
