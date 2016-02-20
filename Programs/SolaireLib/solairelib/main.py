447f820f484285926ad4acb82f68f8cc13ffe94c
Shgck <shgck@pistache.land>
2016-02-20 13:08:39 +0100

    Temporary update SolaireLib main with TPF extraction

--- Programs/SolaireLib/solairelib/main.py
+++ Programs/SolaireLib/solairelib/main.py
@@ -1,7 +1,9 @@
+import sys
+
 from solairelib.tpf import Tpf
 
 
 MUH_TPF = Tpf()
-MUH_TPF.load("/home/shgck/dev/Projets/DarkSoulsDev/Workspace/RootFiles/0/map/m10/m10_9999.tpf")
-MUH_TPF.extract_textures("/home/shgck/dev/Projets/DarkSoulsDev/Workspace")
+MUH_TPF.load(sys.argv[1])
+MUH_TPF.extract_textures(sys.argv[2])
 
